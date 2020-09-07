f10ae296e9a57bed218430e5352747e5d6e66f75
Jaine Saconi <jaine_saconi@hotmail.com>
2020-09-07 17:12:39 -0300

    correção das perguntas

--- src/app/homepage/result/result.component.ts
+++ src/app/homepage/result/result.component.ts
@@ -15,116 +15,120 @@ export class ResultComponent implements OnInit {
 
   resultFinal = '';
   text: string;
+  fogo = 'de fogo';
+  ar = 'de ar';
+  terra = 'de terra';
+  agua = 'de água';
 
   constructor() { }
 
   ngOnInit(): void {
 
     if (this.result == 1)
-      this.resultFinal = 'de ansiedade';
+      this.resultFinal = this.ar;
 
     if (this.result == 2)
-      this.resultFinal = 'de depressão';
+      this.resultFinal = this.ar;
 
     if (this.result == 3)
-      this.resultFinal = 'de stress';
+      this.resultFinal = this.agua;
 
     if (this.result == 4)
-      this.resultFinal = 'de cansaço';
+      this.resultFinal = this.terra;
 
     if (this.result == 5) {
       if (this.xQuestionWater > this.xQuestionAir) {
-        this.resultFinal = 'de água';
+        this.resultFinal = this.agua;
       } else {
-        this.resultFinal = 'de ansiedade';
+        this.resultFinal = this.ar;
       }
     }
 
     if (this.result == 6) {
       if (this.xQuestionWater > this.xQuestionEarth) {
-        this.resultFinal = 'de stress';
+        this.resultFinal = this.agua;
       } else {
-        this.resultFinal = 'de cansaço';
+        this.resultFinal = this.terra;
       }
     }
 
     if (this.result == 7) {
       if (this.xQuestionFire > this.xQuestionEarth) {
-        this.resultFinal = 'de depressão';
+        this.resultFinal = this.ar;
       } else {
-        this.resultFinal = 'de cansaço';
+        this.resultFinal = this.terra;
       }
     }
 
     if (this.result == 8) {
       if (this.xQuestionFire > this.xQuestionEarth) {
-        this.resultFinal = 'de depressão';
+        this.resultFinal = this.ar;
       } else {
-        this.resultFinal = 'de cansaço';
+        this.resultFinal = this.terra;
       }
     }
 
     if (this.result == 9) {
       if (this.xQuestionFire > this.xQuestionWater) {
-        this.resultFinal = 'de depressão';
+        this.resultFinal = this.ar;
       } else {
-        this.resultFinal = 'de stress';
+        this.resultFinal = this.agua;
       }
     }
 
     if (this.result == 10) {
       if (this.xQuestionAir > this.xQuestionEarth) {
-        this.resultFinal = 'de ansiedade';
+        this.resultFinal = this.ar;
       } else {
-        this.resultFinal = 'de cansaço';
+        this.resultFinal = this.terra;
       }
     }
 
     if (this.result == 11) {
       if (this.xQuestionFire >= this.xQuestionAir && this.xQuestionFire >= this.xQuestionWater)
-        this.resultFinal = 'de depressão';
+        this.resultFinal = this.fogo;
       else if (this.xQuestionWater > this.xQuestionFire && this.xQuestionWater >= this.xQuestionEarth)
-        this.resultFinal = 'de stress';
+        this.resultFinal = this.agua;
       else
-        this.resultFinal = 'de cansaço';
+        this.resultFinal = this.terra;
     }
 
     if (this.result == 12) {
       if (this.xQuestionWater >= this.xQuestionAir && this.xQuestionWater >= this.xQuestionEarth)
-        this.resultFinal = 'de stress'
+        this.resultFinal = this.agua;
       else if (this.xQuestionAir > this.xQuestionWater && this.xQuestionAir >= this.xQuestionEarth)
-        this.resultFinal = 'de ansiedade';
+        this.resultFinal = this.ar;
       else
-        this.resultFinal = 'de cansaço';
+        this.resultFinal = this.terra;
     }
 
     if (this.result == 13) {
       if (this.xQuestionFire >= this.xQuestionWater && this.xQuestionFire >= this.xQuestionEarth)
-        this.resultFinal = "de depressão";
+        this.resultFinal = this.fogo;
       else if (this.xQuestionWater > this.xQuestionFire && this.xQuestionWater >= this.xQuestionEarth)
-        this.resultFinal = "de stress";
+        this.resultFinal = this.agua;
       else
-        this.resultFinal = "cansaço";
+        this.resultFinal = this.terra;
     }
 
     if (this.result == 14) {
       if (this.xQuestionFire >= this.xQuestionAir && this.xQuestionFire >= this.xQuestionEarth)
-        this.resultFinal = "de drepresão";
+        this.resultFinal = this.fogo;
       else if (this.xQuestionAir > this.xQuestionFire && this.xQuestionAir >= this.xQuestionEarth)
-        this.resultFinal = "de ansiedade";
+        this.resultFinal = this.ar;
       else
-        this.resultFinal = "de cansaço";
+        this.resultFinal = this.terra;
     }
 
     if (this.result == 15) {
       if (this.xQuestionFire >= this.xQuestionAir && this.xQuestionFire >= this.xQuestionWater && this.xQuestionFire >= this.xQuestionEarth)
-        this.resultFinal = "de depressão";
+        this.resultFinal = this.fogo;
       else if (this.xQuestionWater > this.xQuestionFire && this.xQuestionWater >= this.xQuestionAir && this.xQuestionWater >= this.xQuestionEarth)
-        this.resultFinal = "de stress";
+        this.resultFinal = this.agua;
       else if (this.xQuestionAir > this.xQuestionFire && this.xQuestionAir > this.xQuestionWater && this.xQuestionAir >= this.xQuestionEarth)
-        this.resultFinal = "de ansiedade";
+        this.resultFinal = this.ar;
       else
-        this.resultFinal = "de cansaço";
+        this.resultFinal = this.terra;
     }
 
   }
